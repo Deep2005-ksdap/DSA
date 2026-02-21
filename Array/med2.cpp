23e211fd14f4a296564705e42f29556228517e14
Deepanshu <dk128131@gmail.com>
2026-02-21 13:19:12 +0530

    two sum done

--- Array/med2.cpp
+++ Array/med2.cpp
@@ -1,18 +1,65 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<unordered_map>
 
 using namespace std;
 
 vector<int> twoSumProblem_Brute(vector<int> a, int target);
+vector<int> twoSumProblem_Better(vector<int> a, int target);
+bool twoSumProblem_Optimal(vector<int> a, int target);
 
 int main(){
   vector<int> v = {2,6,5,8,11};
   int target = 14;
+  vector<int> ans = twoSumProblem_Better(v, target);
 
-  twoSumProblem_Brute(v, target) == vector<int>{-1,-1} ? cout << "NO" : cout << "YES";
+  twoSumProblem_Better(v, target) == vector<int>{-1,-1} ? cout << "NO" : cout << "YES" << endl;
+  for(int x : ans) cout << x << " ";
+  cout << endl;
+  twoSumProblem_Optimal(v, target) ? cout << "YES" : cout << "NO" << endl;
 
   return 0;
+}
+bool twoSumProblem_Optimal(vector<int> a, int target){
+  int n = a.size();
+  vector<pair<int, int>> arrWithIndices;
+
+  for(int i = 0; i < n; i++){
+    arrWithIndices.push_back({a[i], i});
+  }
+
+  sort(arrWithIndices.begin(), arrWithIndices.end());
+
+  int left = 0, right = n -1 ;
+  while(left < right){
+    int sum = arrWithIndices[left].first + arrWithIndices[right].first;
+    if(sum == target){
+      return true;
+    }else if(sum < target){
+      left++;
+    }else if(sum > target){
+      right--;
+    }
+  }
+  
+  return false;
+}
+
+vector<int> twoSumProblem_Better(vector<int> a, int target){
+  unordered_map<int, int> mp;
+  int n = a.size();
+
+  for(int i = 0; i < n; i++){
+    int diff = target - a[i];
+
+    if(mp.find(diff) != mp.end()){
+      return {mp[diff], i};
+    }
+    mp[a[i]] = i;
+  }
+
+  return {-1, 1};
 }
 
 vector<int> twoSumProblem_Brute(vector<int> a, int target){
